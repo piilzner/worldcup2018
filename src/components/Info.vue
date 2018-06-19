8ad6e00815c3f90cd191762199f3811073595041
Nils Löfgren <nilslofgren@Nilss-MacBook-Pro.local>
2018-06-19 22:05:56 +0200

    added mobile-styling and bottom link

---
+++ src/components/Info.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="nav">
+    <a href="https://erepizzafredag.se/">Gå tillbaka till erepizzafredag!</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Info',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+.nav {
+  display: block;
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 40px 0;
+  box-sizing: border-box;
+}
+
+.nav a:hover {
+  color: #fff;
+}
+</style>
